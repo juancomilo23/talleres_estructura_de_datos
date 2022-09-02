922d0f2e395fe7a59510a709ccb6e284fc174814
juan camilo ospina chacon <88067572+juancomilo23@users.noreply.github.com>
2022-09-02 16:09:07 -0500

    Update Eje#2.kt

--- taller 6/Eje#2.kt
+++ taller 6/Eje#2.kt
@@ -1,48 +1,87 @@
-package Solucion_taller_6
+package Taller06
 
-class asensor{
-    private var pisos:Int=0
-    private var sotanos:Int=0
-    private var pactual:Int=0
-    constructor() //primario
-    constructor(pisos: Int, sotanos: Int, pactual: Int) {
-        this.pisos = pisos
-        this.sotanos = sotanos
-        this.pactual = pactual
+class Ascensor {
+    //Atributos de la clase
+    private var numPisos : Int = 0
+    private var numSotanos : Int = 0
+    private var pActual : Int = 1
+
+    //Constructor primario
+    constructor()
+
+    //Constructor secundario
+    constructor(pActual: Int, numPisos:Int, numSotanos:Int){
+        this.pActual= pActual
+        this.numPisos = numPisos
+        this.numSotanos = numSotanos
     }
-    //analizadores get
-    fun getpisos()=this.pisos
-    fun getsotanos()=this.sotanos
-    fun getpactual()=this.pactual
-    //modificadores set
-    fun setp(nuevo:Int){
-        if(nuevo!=0){
-            this.pisos=nuevo
-        }
+
+    fun getnumPisos()= this.numPisos
+    fun getnumSotanos()= this.numSotanos
+    fun getpActual()=this.pActual
+
+    fun setnumPisos(nuevoValor: Int){
+        this.numPisos=nuevoValor
     }
-    fun sets(nuevo_a:Int){
-        if(nuevo_a!=0){
-            this.sotanos=nuevo_a
-        }
+    fun setnumSotanos(nuevoValor: Int){
+        this.numPisos=nuevoValor
     }
-    fun setpa(nuevo_b:Int){
-        if(nuevo_b!=0){
-            this.pactual=nuevo_b
-        }
+    fun setpActual(nuevoValor: Int){
+        this.numPisos=nuevoValor
     }
-    //metodos
-    fun primera():Int{
-        require(this.pactual<this.pisos)
-        if(this.pactual<this.pisos){
-            if(this.pactual==-1){
-                this.pactual=this.pactual+2
+    fun atender() =this.numPisos+this.numSotanos
+
+    //Métodos que determina el piso en el que quedará
+    fun deterPiso1(){
+        if (pActual<numPisos ){
+            when (this.pActual) {
+                -1-> this.pActual=1
+                else -> this.pActual++
             }
-            else{
-                this.pactual=this.pactual+1
-            }
-
         }
-        return this.pactual
+        else {
+            println("El ascensor se encuentre en el último piso ")
+        }
     }
 
+    fun deterPiso2() {
+        if (this.pActual>-this.numSotanos){
+            when (this.pActual){
+                1-> this.pActual=-1
+                else-> this.pActual--
+            }
+        }
+    }
+
+}
+fun main(){
+    var asc = Ascensor()
+    var opc = 0
+    do{
+        println("0. Salir\n1. Crear\n2. Subir\n3. Bajar\n4. Num pisos\n5. piso actual")
+        print("Escoja una opción: ")
+        opc = readLine()!!.toInt()
+        when (opc){
+            1-> {
+                print("Entre el piso actual: ")
+                val pActual = readLine()!!.toInt()
+                print("Entre el número de pisos: ")
+                val numPisos = readLine()!!.toInt()
+                print("Entre el número de sótanos: ")
+                val numSotanos = readLine()!!.toInt()
+                asc = Ascensor(pActual,numPisos,numSotanos)
+                println("Ascensor creado con éxito! ")
+            }
+            2-> {
+                asc.deterPiso1()
+                println("El piso al que sube es ${asc.getpActual()}  ")
+            }
+            3->{
+                asc.deterPiso2()
+                println("El piso al que baja es ${asc.getpActual()}  ")
+            }
+            4-> println("El número de pisos que puede atender el ascensor es de ${asc.atender()}")
+            5-> println("El número en el que se encuentra es ${asc.getpActual()} ")
+        }
+    }while (opc != 0)
 }
